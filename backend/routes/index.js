4a4a8ecea29e3e7da455a71d684ddca3ca513d02
Prashant saxena <62493476+Prashant-superior@users.noreply.github.com>
2024-07-06 21:02:51 +0530

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,11 @@
 
 const express = require("express");
-// const  signinRouter = require("./signin");
+const  signinRouter = require("./user");
 // const watchlistRouter = require("./watchlist");
 const homeRouter = require("./home");
 
 const router = express.Router();
-// router.use("/signin" , signinRouter);
+router.use("/user" , signinRouter);
 // router.use("/watchlist", watchlistRouter);
 router.use("/home", homeRouter );
 
