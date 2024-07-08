d673f363791ac7a7dc5d2577164218e46b7b021e
Prashant saxena <62493476+Prashant-superior@users.noreply.github.com>
2024-07-08 14:51:12 +0530

    Create tempCodeRunnerFile.js

---
+++ frontend/src/assets/tempCodeRunnerFile.js
@@ -0,0 +1,14 @@
+
+	// import axios from "axios"
+
+	// const fetchedData = async () => {
+	// 	const response = await axios.get("https://api.themoviedb.org/3/trending/movie/day?language=en-US", {
+	// 		headers: {
+	// 		  accept: 'application/json',
+	// 		  Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNWM3ZmRlYjU1ZWM5OTFjZmRjNDg3MzljOGY0MzQwYSIsIm5iZiI6MTcyMDM4MjA4OC42ODQ5NTUsInN1YiI6IjY2ODdiNTVkZDk1MzFlMjQ1YjA4MTM0MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JOnZADqsJ7gf0i3jHcVY0t_XrtW3CKNSzAyPkn0hxzM'
+	// 		}
+	// 	});
+	// 	console.log(response.data.results);
+	// 	return response.data;	
+	// }
+	// export default fetchedData;
