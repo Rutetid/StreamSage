133fbaa20c61d50c966c66eb1e4263abb53f24fe
Rutetid <50543186+Rutetid@users.noreply.github.com>
2024-07-10 01:29:57 +0530

    minor change

--- frontend/src/Components/Landing Page/Login.jsx
+++ frontend/src/Components/Landing Page/Login.jsx
@@ -36,7 +36,7 @@ const Login = () => {
 						<div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-bglight dark:border-gray-900">
 							<div className="p-6 space-y-4 md:space-y-6 sm:p-8">
 								<h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-									Sign in to your account
+									Login to your account
 								</h1>
 								<form className="space-y-4 md:space-y-6" action="#" onSubmit={handleSubmit}>
 									<div>
@@ -50,7 +50,7 @@ const Login = () => {
 											type="email"
 											name="email"
 											id="email"
-											className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-text dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+											className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-text dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
 											placeholder="name@company.com"
 											required=""
 											onChange={(e)=>setUsername(e.target.value)}
@@ -69,7 +69,7 @@ const Login = () => {
 											name="password"
 											id="password"
 											placeholder="••••••••"
-											className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-text dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+											className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-text dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
 											required=""
 											onChange={(e)=>setPassword(e.target.value)}
 										/>
