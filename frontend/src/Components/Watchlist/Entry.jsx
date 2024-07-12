f83db7ffc59c16168d9afdd853405f9c05219992
Rutetid <50543186+Rutetid@users.noreply.github.com>
2024-07-13 01:34:24 +0530

    get backend watlist data

--- frontend/src/Components/Watchlist/Entry.jsx
+++ frontend/src/Components/Watchlist/Entry.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
-import got from '../../assets/got.jpg'
+import React from "react";
+import got from "../../assets/got.jpg";
+import axios from "axios";
 
 const Entry = () => {
-  return (
-			<div>
-				<div className="h-40 bg-text mx-48 flex items-center font-poppins font-bold text-2xl">
-					<div className="w-1/12 flex justify-center">1</div>
-					<div className="w-1/12">
-						<img src={got} alt="" className=" flex justify-start object-contain h-36" />
-					</div>
-					<div className="w-6/12 pl-12">Game of Thrones</div>
-					<div className="w-2/12 pl-5 ">9.0</div>
-					<div className="w-2/12 pl-5">seen</div>
-				</div>
-			</div>
-		);
-}
+	const token = localStorage.getItem("token");
+	const response = async () => {
+		axios.get("http://localhost:3000/api/v1/watchlist/get", {
+			headers: {
+				"Content-Type": "application/json",
+				authorization: `Bearer ${token}`,
+			},
+		});
+		const movies = await response.data;
+		console.log(movies);
+	};
 
-export default Entry
+	return (
+		<div>
+			<div className="h-40 bg-text mx-48 flex items-center font-poppins font-bold text-2xl">
+				<div className="w-1/12 flex justify-center">1</div>
+				<div className="w-1/12">
+					<img
+						src={got}
+						alt=""
+						className=" flex justify-start object-contain h-36"
+					/>
+				</div>
+				<div className="w-6/12 pl-12">Game of Thrones</div>
+				<div className="w-2/12 pl-5 ">9.0</div>
+				<div className="w-2/12 pl-5">seen</div>
+			</div>
+		</div>
+	);
+};
+
+export default Entry;
