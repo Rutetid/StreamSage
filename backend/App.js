66c4af83fdcfd31d86a1cafd0ab43faaeb3e865c
Rutetid <50543186+Rutetid@users.noreply.github.com>
2024-07-16 21:14:26 +0530

    trying to deploy

--- backend/App.js
+++ backend/App.js
@@ -5,13 +5,13 @@ const cors = require("cors");
 const mainRouter = require("./routes/index")
 
 app.use(express.json());
-app.use(
-	cors({
-		origin: "https://streamsage.vercel.app",
-		methods: ["GET", "POST", "PUT", "DELETE"],
-		credentials: true,
-	}),
-);
+app.use();
+	// cors({
+	// 	origin: "https://streamsage.vercel.app",
+	// 	methods: ["GET", "POST", "PUT", "DELETE"],
+	// 	credentials: true,
+	// }),
+
 
 app.use("/api/v1", mainRouter);
 
