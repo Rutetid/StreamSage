dd907cdf1fc10405b200cc88f48948ce4922d454
Rutetid <50543186+Rutetid@users.noreply.github.com>
2024-07-16 15:49:58 +0530

    trying to deploy

--- backend/App.js
+++ backend/App.js
@@ -18,3 +18,7 @@ app.use("/api/v1", mainRouter);
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
   });
+
+app.get("/", (req, res) => {
+    res.json("Hello World");
+});
